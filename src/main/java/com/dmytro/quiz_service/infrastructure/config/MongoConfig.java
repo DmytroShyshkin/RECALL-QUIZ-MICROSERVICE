27eacefe31bd0c132a54b19bca6f4613b8864ef6
DmytroShishkin NC <dmytro.dev.shyshkin@gmail.com>
2026-07-10 13:52:05 +0200

    Remove unused MongoDB UUID representation config
    The mongoClientSettings bean that configured UUID representation to STANDARD is no longer needed. The default MongoDB behavior is sufficient for this service.

--- src/main/java/com/dmytro/quiz_service/infrastructure/config/MongoConfig.java
+++ src/main/java/com/dmytro/quiz_service/infrastructure/config/MongoConfig.java
@@ -1,8 +1,5 @@
 package com.dmytro.quiz_service.infrastructure.config;
 
-import com.mongodb.MongoClientSettings;
-import org.bson.UuidRepresentation;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -11,11 +8,4 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoAuditing
 @EnableMongoRepositories(basePackages = "com.dmytro.quiz_service.infrastructure.persistence")
 public class MongoConfig {
-
-    @Bean
-    public MongoClientSettings mongoClientSettings() {
-        return MongoClientSettings.builder()
-                .uuidRepresentation(UuidRepresentation.STANDARD)
-                .build();
-    }
 }
