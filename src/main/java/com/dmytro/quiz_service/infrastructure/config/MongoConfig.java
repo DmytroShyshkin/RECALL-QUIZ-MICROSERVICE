d368a204197fc8481f5736d16b038caed4bdfc88
DmytroShishkin NC <dmytro.dev.shyshkin@gmail.com>
2026-07-10 14:02:55 +0200

    Update MongoConfig.java

--- src/main/java/com/dmytro/quiz_service/infrastructure/config/MongoConfig.java
+++ src/main/java/com/dmytro/quiz_service/infrastructure/config/MongoConfig.java
@@ -2,10 +2,8 @@ package com.dmytro.quiz_service.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @EnableMongoAuditing
-@EnableMongoRepositories(basePackages = "com.dmytro.quiz_service.infrastructure.persistence")
 public class MongoConfig {
 }
