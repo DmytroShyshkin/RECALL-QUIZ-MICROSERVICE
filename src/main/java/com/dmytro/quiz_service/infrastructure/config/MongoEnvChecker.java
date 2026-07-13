b9c00a4522060ef99ef978dadc3786919fd48b3b
DmytroShishkin NC <dmytro.dev.shyshkin@gmail.com>
2026-07-13 10:32:54 +0200

    Create MongoEnvChecker.java

---
+++ src/main/java/com/dmytro/quiz_service/infrastructure/config/MongoEnvChecker.java
@@ -0,0 +1,23 @@
+package com.dmytro.quiz_service.infrastructure.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MongoEnvChecker {
+
+    @Autowired
+    private Environment env;
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onReady() {
+        String uri = env.getProperty("spring.data.mongodb.uri");
+        boolean fromEnv = System.getenv("MONGODB_URI") != null;
+        String masked = uri == null ? "null" : uri.replaceAll(":(.*)@", ":*****@");
+        System.out.println("spring.data.mongodb.uri resolved: " + masked);
+        System.out.println("MONGODB_URI present in env: " + fromEnv);
+    }
+}
